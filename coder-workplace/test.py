6fc2d3b691ee152603224975b2cf1a0a3cedf744
Konstantin Neradovsky <kneradovsky@gmail.com>
2021-12-29 17:20:57 +0300

    new script

--- coder-workplace/test.py
+++ coder-workplace/test.py
@@ -7,16 +7,20 @@ y=0
 a=0
 t.setpos(0,0)
 t.tracer(0,5)
-for j in range(100):
+t.colormode(255)
+for j in range(200):
     t.pu()
     t.home()
     t.seth(a % 360)
     t.fd(30)
     t.pd()
-    a+=17
+    if j>72 : a+=-5
+    else : a+=5
     for i in range(4) :
         t.fd(25)
         t.rt(90)
+        t.pencolor((j%10*20,j%128*2,j%64*4))
+
     t.update()
 
-sys.stdin.read()
+sys.stdin.readline()    
